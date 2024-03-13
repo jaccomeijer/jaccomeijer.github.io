8e8fb7947864c2a28f56d5c74a5b521806b52338
Jacco Meijer <jacco@jaccomeijer.nl>
2024-03-14 00:15:55 +0100

    chore: remove double gutters

---
+++ src/layout/blog-layout.jsx
@@ -0,0 +1,36 @@
+import { getOpenerTopics, addTopicLabels } from '../pages/blog/get-opener-topics.js'
+import { globals } from '../config/globals.js'
+import { blogImages } from '../pages/blog/blog-images.js'
+import { Topic, TopicList } from '@jaccomeijer/green-lib'
+import { BaseLayout } from './base-layout.jsx'
+
+export const BlogLayout = props => {
+  return (
+    <BaseLayout {...props}>
+      <div class="s-wrapper markdown">
+        <Topic
+          data-variant="headline"
+          globals={globals}
+          images={blogImages}
+          swap-image={true}
+          topic={addTopicLabels({ topic: props.page.frontmatter.topics.opener, page: props.page })}
+        />
+      </div>
+      <div class="s-wrapper" style="--wrapper-width: 45rem">
+        <article class="markdown s-flex-column u-gap-0">
+          {props.children}
+        </article>
+      </div>
+      <div class="s-wrapper markdown">
+        <hr />
+        <TopicList
+          class="s-grid u-grid-three-cols u-gap-m"
+          data-variant="card"
+          globals={globals}
+          images={blogImages}
+          topics={getOpenerTopics({ pages: props.pages, tag: 'blog' })}
+        />
+      </div>
+    </BaseLayout>
+  )
+}
