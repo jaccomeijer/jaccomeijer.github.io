6fd61a5984b02d54d62a749ad6547a3b416e61f1
Jacco Meijer <jacco@jaccomeijer.nl>
2024-03-15 11:41:19 +0100

    refactor: order blog by date

--- src/pages/blog/blog-images.js
+++ src/pages/blog/blog-images.js
@@ -1,17 +1,16 @@
-import agileScrum from './agile-scrum/solen-feyissa-zhmO6rGFlGI-unsplash.jpg'
-import comparingBetweenReactAppNextjsAndGatsby from './comparing-between-react-app-nextjs-and-gatsby/solen-feyissa-mfrDGn5juqs-unsplash.jpg'
-import contentfulNetlifyAndGatsbyFourYearsLater from './contentful-netlify-and-gatsby-four-years-later/sharon-pittaway-4_hFxTsmaO4-unsplash.jpg'
-import contentModelingWithVariants from './content-modeling-with-variants/simon-lee-zft-W1kVEhg-unsplash.jpg'
-import documentation from './documentation/richard-horvath-_nWaeTF6qo0-unsplash.jpg'
-import javascriptHistory from './javascript-history/rianne-zuur-UTZsyY9k5MQ-unsplash.jpg'
-import modelberry from './modelberry/rianne-zuur-PoxFTU7_QKo-unsplash.jpg'
-import namingReactComponents from './naming-react-components/oil-ge6fc151db_1920.jpg'
-import npm7AndNpmcliArborist from './npm-7-and-npmcli-arborist/clark-van-der-beken-xApC8DIiD54-unsplash.jpg'
-import onJavascriptTranspilersBundlersAndModules from './on-javascript-transpilers-bundlers-and-modules/ari-spada-wVoMvN5NsU0-unsplash.jpg'
-import reactComponentThemesAndCssVariables from './react-component-themes-and-css-variables/ben-abo-J8GGXy4fY88-unsplash.jpg'
-import typescriptInterfaceForReactUiComponents from './typescript-interface-for-react-ui-components/kai-dahms-97yTozZh7N8-unsplash.jpg'
-import versioningStrategyForNpmModules from './versioning-strategy-for-npm-modules/mae-mu-WqQwOQ9fVj4-unsplash.jpg'
-import whatHappenedToWheelroom from './what-happened-to-wheelroom/daniel-olah-VS_kFx4yF5g-unsplash.jpg'
+import agileScrum from './2022-02 - agile-scrum/solen-feyissa-zhmO6rGFlGI-unsplash.jpg'
+import comparingBetweenReactAppNextjsAndGatsby from './2022-05 - comparing-between-react-app-nextjs-and-gatsby/solen-feyissa-mfrDGn5juqs-unsplash.jpg'
+import contentfulNetlifyAndGatsbyFourYearsLater from './2022-01 - contentful-netlify-and-gatsby-four-years-later/sharon-pittaway-4_hFxTsmaO4-unsplash.jpg'
+import contentModelingWithVariants from './2022-03 - content-modeling-with-variants/simon-lee-zft-W1kVEhg-unsplash.jpg'
+import documentation from './2022-03 - documentation/richard-horvath-_nWaeTF6qo0-unsplash.jpg'
+import javascriptHistory from './2022-03 - javascript-history/rianne-zuur-UTZsyY9k5MQ-unsplash.jpg'
+import namingReactComponents from './2022-01 - naming-react-components/oil-ge6fc151db_1920.jpg'
+import npm7AndNpmcliArborist from './2022-06 - npm-7-and-npmcli-arborist/clark-van-der-beken-xApC8DIiD54-unsplash.jpg'
+import onJavascriptTranspilersBundlersAndModules from './2022-03 - on-javascript-transpilers-bundlers-and-modules/ari-spada-wVoMvN5NsU0-unsplash.jpg'
+import reactComponentThemesAndCssVariables from './2022-04 - react-component-themes-and-css-variables/ben-abo-J8GGXy4fY88-unsplash.jpg'
+import typescriptInterfaceForReactUiComponents from './2022-01 - typescript-interface-for-react-ui-components/kai-dahms-97yTozZh7N8-unsplash.jpg'
+import versioningStrategyForNpmModules from './2022-05 - versioning-strategy-for-npm-modules/mae-mu-WqQwOQ9fVj4-unsplash.jpg'
+import whatHappenedToWheelroom from './2022-01 - what-happened-to-wheelroom/daniel-olah-VS_kFx4yF5g-unsplash.jpg'
 
 export const blogImages = {
   agileScrum,
@@ -20,7 +19,6 @@ export const blogImages = {
   contentModelingWithVariants,
   documentation,
   javascriptHistory,
-  modelberry,
   namingReactComponents,
   npm7AndNpmcliArborist,
   onJavascriptTranspilersBundlersAndModules,
