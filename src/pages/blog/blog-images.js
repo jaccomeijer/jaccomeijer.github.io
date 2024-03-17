b0af8e96d88743a96b6754913d9927dd0f366bd6
Jacco Meijer <jacco@jaccomeijer.nl>
2024-03-17 23:55:15 +0100

    chore: setup draft posts on 11ty, esbuild and jsx

--- src/pages/blog/blog-images.js
+++ src/pages/blog/blog-images.js
@@ -1,8 +1,11 @@
 import agileScrum from './2022-02 - agile-scrum/solen-feyissa-zhmO6rGFlGI-unsplash.jpg'
+import cissp from './2024-03 - owasp-cissp/rianne-zuur-PoxFTU7_QKo-unsplash.jpg'
 import comparingBetweenReactAppNextjsAndGatsby from './2022-05 - comparing-between-react-app-nextjs-and-gatsby/solen-feyissa-mfrDGn5juqs-unsplash.jpg'
 import contentfulNetlifyAndGatsbyFourYearsLater from './2022-01 - contentful-netlify-and-gatsby-four-years-later/sharon-pittaway-4_hFxTsmaO4-unsplash.jpg'
 import contentModelingWithVariants from './2022-03 - content-modeling-with-variants/simon-lee-zft-W1kVEhg-unsplash.jpg'
 import documentation from './2022-03 - documentation/richard-horvath-_nWaeTF6qo0-unsplash.jpg'
+import esbuildAsStaticWebsiteGenerator from './2024-03 - esbuild-as-static-website-generator/jj-ying-7JX0-bfiuxQ-unsplash.jpg'
+import etyAndJsx from './2024-03 - 11ty-and-jsx/jean-philippe-delberghe-75xPHEQBmvA-unsplash.jpg'
 import javascriptHistory from './2022-03 - javascript-history/rianne-zuur-UTZsyY9k5MQ-unsplash.jpg'
 import namingReactComponents from './2022-01 - naming-react-components/oil-ge6fc151db_1920.jpg'
 import npm7AndNpmcliArborist from './2022-06 - npm-7-and-npmcli-arborist/clark-van-der-beken-xApC8DIiD54-unsplash.jpg'
@@ -11,14 +14,16 @@ import reactComponentThemesAndCssVariables from './2022-04 - react-component-the
 import typescriptInterfaceForReactUiComponents from './2022-01 - typescript-interface-for-react-ui-components/kai-dahms-97yTozZh7N8-unsplash.jpg'
 import versioningStrategyForNpmModules from './2022-05 - versioning-strategy-for-npm-modules/mae-mu-WqQwOQ9fVj4-unsplash.jpg'
 import whatHappenedToWheelroom from './2022-01 - what-happened-to-wheelroom/daniel-olah-VS_kFx4yF5g-unsplash.jpg'
-import cissp from './2024-03 - owasp-cissp/rianne-zuur-PoxFTU7_QKo-unsplash.jpg'
 
 export const blogImages = {
   agileScrum,
+  cissp,
   comparingBetweenReactAppNextjsAndGatsby,
   contentfulNetlifyAndGatsbyFourYearsLater,
   contentModelingWithVariants,
   documentation,
+  esbuildAsStaticWebsiteGenerator,
+  etyAndJsx,
   javascriptHistory,
   namingReactComponents,
   npm7AndNpmcliArborist,
@@ -27,5 +32,4 @@ export const blogImages = {
   typescriptInterfaceForReactUiComponents,
   versioningStrategyForNpmModules,
   whatHappenedToWheelroom,
-  cissp,
 }
